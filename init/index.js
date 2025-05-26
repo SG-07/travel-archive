175cc83b6a9076df8474d14f2f7b962b00967eff
Satyam Gangwar <gangwar.satyam01@gmail.com>
2025-05-26 15:49:15 +0530

    Initial Setup, with data

---
+++ init/index.js
@@ -0,0 +1,34 @@
+require('dotenv').config({ path: '../.env' });
+const mongoose = require('mongoose');
+const initData = require('./data.js');
+const Listing = require('../models/listing.js');
+
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected to MongoDB and using database:", mongoose.connection.name))
+  .catch(err => console.error("MongoDB connection error:", err));
+
+
+
+//disconneting from db
+process.on('SIGINT', async () => {
+    try {
+        await mongoose.connection.close();
+        console.log("MongoDB connection closed.");
+        process.exit(0);
+    } catch (err) {
+        console.error("Error closing MongoDB connection:", err);
+        process.exit(1);
+    }
+});
+
+
+const initDB = async () => {
+    mongoose.connection.once('open', async () => {
+        console.log("Connected to MongoDB, now adding listings...");
+        await Listing.insertMany(initData.data);
+        console.log("Listings addd successfully.");
+    });
+};
+
+initDB();
